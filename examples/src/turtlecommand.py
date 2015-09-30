596fa9cbf49dd546a12ea8ae3878fb8428b36fe4
alexv <asmodehn@gmail.com>
2015-09-30 14:00:27 +0900

    first version of QT UI for turtle command center

---
+++ examples/src/turtlecommand.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+import sys
+from PyQt4 import QtCore, QtGui
+from turtlecmd_ui import Ui_MainWindow
+
+
+class MyWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyWindow()
+    myapp.show()
+    sys.exit(app.exec_())
