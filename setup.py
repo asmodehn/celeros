0e115a390d61722306b4032790095b54442eb00e
alexv <asmodehn@gmail.com>
2015-10-08 14:38:43 +0900

    adding flower as dependency for celeros

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except Exception, e:
     from setuptools import setup
 
-# CAREFUL distutils and setuptools take different argument sand have different behaviors
+# CAREFUL distutils and setuptools take different arguments and have different behaviors
 if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
     # fetch values from package.xml
     setup_args = generate_distutils_setup(
@@ -18,12 +18,17 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
             'celery', 'celery.app', 'celery.apps', 'celery.backends', 'celery.backends.database', 'celery.bin', 'celery.concurrency', 'celery.contrib', 'celery.events', 'celery.fixups', 'celery.loaders', 'celery.security', 'celery.task', 'celery.utils', 'celery.utils.dispatch', 'celery.worker',
             'kombu', 'kombu.async', 'kombu.transport', 'kombu.transport.sqlalchemy', 'kombu.transport.virtual', 'kombu.utils',
             'billiard', 'billiard.dummy', 'billiard.py2', 'billiard.py3',
+            'flower', 'flower.api', 'flower.utils', 'flower.utils.backports', 'flower.views',
         ],
         package_dir={
             'celeros': 'celeros',
-            'celery': 'deps/celery/celery',
+            'celery': 'deps/celery/celery', # for devel : 'deps/celery' / for install : 'deps/celery/celery'. hopefully ros-indigo-catkin will fix this from 0.6.15
             'kombu': 'deps/kombu/kombu',
             'billiard': 'deps/billiard/billiard',
+            'flower': 'deps/flower/flower',
+        },
+        package_data={
+            'flower': ['templates/*', 'static/**/*', 'static/*.*']
         },
     )
     setup(**setup_args)
@@ -43,12 +48,17 @@ else:  # using setuptools : http://pythonhosted.org/setuptools/
             'celery', 'celery.app', 'celery.apps', 'celery.backends', 'celery.backends.database', 'celery.bin', 'celery.concurrency', 'celery.contrib', 'celery.events', 'celery.fixups', 'celery.loaders', 'celery.security', 'celery.task', 'celery.utils', 'celery.utils.dispatch', 'celery.worker',
             'kombu', 'kombu.async', 'kombu.transport', 'kombu.transport.sqlalchemy', 'kombu.transport.virtual', 'kombu.utils',
             'billiard', 'billiard.dummy', 'billiard.py2', 'billiard.py3',
+            'flower', 'flower.api', 'flower.utils', 'flower.utils.backports', 'flower.views',
         ],
         package_dir={
             'celeros': 'celeros',
             'celery': 'deps/celery/celery',
             'kombu': 'deps/kombu/kombu',
             'billiard': 'deps/billiard/billiard',
+            'flower': 'deps/flower/flower',
+        },
+        package_data={
+            'flower': ['templates/*', 'static/**/*', 'static/*.*']
         },
         # this is better than using package data ( since behavior is a bit different from distutils... )
         include_package_data=True,  # use MANIFEST.in during install.
