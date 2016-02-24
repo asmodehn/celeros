a823c4b4d91d5978d43c23bce1cb8614e0e97ebe
alexv <asmodehn@gmail.com>
2016-02-24 17:09:52 +0900

    cosmetics

--- celeros/rosstart.py
+++ celeros/rosstart.py
@@ -9,10 +9,11 @@ import multiprocessing
 from celery import Celery, bootsteps
 
 
-class BootRostfulNode(bootsteps.StartStopStep):
+# TODO : configuration for tests...
+class BootPyrosNode(bootsteps.StartStopStep):
 
     def __init__(self, worker, **kwargs):
-        # TODO : super(BootRostfulNode, self).__init__()
+        logging.warn('{0!r} is starting from {1}'.format(worker, __file__))
 
         # dynamic setup and import ( in different process now )
         try:
@@ -20,19 +21,28 @@ class BootRostfulNode(bootsteps.StartStopStep):
             import sys
             import pyros
             import pyros.rosinterface
-            # this doesnt work ??
-            #sys.modules["pyros.rosinterface"] = pyros.rosinterface.delayed_import_auto(distro='indigo', base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-            pyros.rosinterface = pyros.rosinterface.delayed_import_auto(distro='indigo', base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+            # this doesnt work with celery handling of imports
+            # sys.modules["pyros.rosinterface"] = pyros.rosinterface.delayed_import_auto(
+            #     distro='indigo',
+            #     base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..')
+            # )
+            pyros.rosinterface = pyros.rosinterface.delayed_import_auto(
+                distro='indigo',
+                base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..')
+            )
         except ImportError as e:
             logging.warn("{name} Error: Could not import pyros.rosinterface : {e}".format(name=__name__, e=e))
             raise
 
         self.ros_argv = kwargs['ros_arg'] if 'ros_arg' in kwargs else []
-        self.node_proc = pyros.rosinterface.PyrosROS('celeros', self.ros_argv, base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+        self.node_proc = pyros.rosinterface.PyrosROS(
+            'celeros',
+            self.ros_argv,
+            base_path=os.path.join(os.path.dirname(__file__), '..', '..', '..')
+        )
         client_conn = self.node_proc.start()
-        worker.app.ros_node_client = pyros.PyrosClient(client_conn)  # we do this in init so all pool processes have acces to it.
-        logging.warn('{0!r} is starting'.format(worker))
-        logging.warn("finished boot init")
+        # we do this in init so all pool processes have access to it.
+        worker.app.ros_node_client = pyros.PyrosClient(client_conn)
 
     def create(self, worker):
         return self
