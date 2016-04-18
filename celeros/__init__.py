2ede464857a476318aa7565df189c5a3f990cf7d
alexv <asmodehn@gmail.com>
2016-04-18 21:40:25 +0900

    exposing datetime encoder and decoder. sometime useful to have.

--- celeros/__init__.py
+++ celeros/__init__.py
@@ -2,9 +2,12 @@ from __future__ import absolute_import
 
 from .rosperiodictasks import PeriodicTask
 from .scheduler import RedisScheduler, RedisScheduleEntry
+from .celerybeatredis import DateTimeEncoder, DateTimeDecoder
 from .app import celeros_app
 
 __all__ = [
+    'DateTimeDecoder',
+    'DateTimeEncoder',
     'PeriodicTask',
     'RedisScheduler',
     'RedisScheduleEntry',
